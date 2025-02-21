528bd6c043a1589e2f5ee71899093b2e32566398
1to3for5vi7ate9x <ankitc7777777@gmail.com>
2025-02-21 23:29:32 +0530

    added the sh

--- shell.sh
+++ shell.sh
@@ -1,3 +1,6 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+set -e
+STATE_FILE="venv/lib/python*/site-packages/discord/state.py"
+sed -i "s/self.pending_payments = {int(p\['id'\]): Payment(state=self, data=p) for p in data.get('pending_payments', \[\])}/self.pending_payments = {int(p\['id'\]): Payment(state=self, data=p) for p in (data.get('pending_payments') or [])}/" $STATE_FILE
